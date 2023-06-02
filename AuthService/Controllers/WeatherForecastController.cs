586ea54c51c406c848fa26e59938b3c838df0a3e
Rayan <ryanfrhat4@gmail.com>
2023-06-02 18:49:06 +0300

    init first service

---
+++ AuthService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers;
+
+/// <summary>
+/// Controller for managing weather forecasts.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    /// <summary>
+    /// Controller for managing weather forecasts.
+    /// </summary>
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Controller for managing weathfffffffffffffffer forecasts.
+    /// </summary>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
