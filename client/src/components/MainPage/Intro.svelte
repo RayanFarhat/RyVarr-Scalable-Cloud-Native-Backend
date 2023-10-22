e1fc74d8549d4c6ceead7979ad27b3cf1f13e8ad
rayanfarhat <rayanfarhat.sh@gmail.com>
2023-10-22 06:06:18 +0300

    add Services Component

--- client/src/components/MainPage/Intro.svelte
+++ client/src/components/MainPage/Intro.svelte
@@ -3,7 +3,7 @@
         <!-- first col -->
         <div class="mockup-window shadow-primary shadow-2xl bg-base-300 w-2/4 py-10">
             <div class="flex justify-center">
-            <img class="w-1/3 h-1/3" src="/images/house_plan_icon.png" alt="house plan icon"/>
+            <img class="w-1/3 h-1/3" src="/icons/house_plan.png" alt="house plan icon"/>
             </div>
           </div>
       <!-- second col -->
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-    import Intro from "../components/Intro.svelte";
+    import Intro from "../components/MainPage/Intro.svelte";
+    import Services from "../components/MainPage/Services.svelte";
 
 </script>
 <Intro/>
+<Services/>
