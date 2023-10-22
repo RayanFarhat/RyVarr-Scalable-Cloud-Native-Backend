8be715bfd3dc9ade9e2fab06faced72a7f342b3e
rayanfarhat <rayanfarhat.sh@gmail.com>
2023-10-22 21:47:36 +0300

    add Why Us section component

---
+++ client/src/components/MainPage/WhyUs.svelte
@@ -0,0 +1,45 @@
+<div class="hero bg-base-100">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Why Us?</h1>
+        <p class="py-6">We're not merely another software agency; we are a startup with the capacity to handle substantial projects 
+            for major corporations. However, our primary mission is to assist individual clients in creating customized software 
+            solutions tailored to their specific requirements and sharing these solutions with others who may also find them beneficial.</p>
+       <h5 class="text-start text-lg text-transparent bg-clip-text bg-gradient-to-r from-primary to-white">We provide two distinct engagement models to cater to your software needs:</h5>
+      </div>
+    </div>
+</div>
+<div class="flex flex-col w-full lg:flex-row justify-center items-center">
+    <div class="card w-96 bg-base-200 border-2 border-primary">
+        <div class="card-body">
+          <h2 class="card-title">Build your software</h2>
+          <p>You have the ownership of the software.</p>
+          <p class="text-lg text-success">Benefits :</p>
+          <div class="chat chat-start">
+              <div class="chat-bubble chat-bubble-success mb-1">The software/website is all yours.</div>
+            </div>
+            <p class="text-lg text-error">Drawbacks :</p>
+            <div class="chat chat-start">
+                <div class="chat-bubble chat-bubble-error mb-1">Future support require additional fees.</div>
+              </div>
+        </div>
+      </div>
+    <div class="divider lg:divider-horizontal">OR</div> 
+    <div class="card w-96 bg-base-200 border-2 border-primary">
+        <div class="card-body">
+          <h2 class="card-title">Build your Feature</h2>
+          <p>We build your new feature from our existing products for you. 
+            The software remains in our ownership.</p>
+            <p class="text-lg text-success">Benefits :</p>
+            <div class="chat chat-start">
+                <div class="chat-bubble chat-bubble-success mb-1">Reduced Price!</div>
+                <div class="chat-bubble chat-bubble-success mb-1">You have LifeTime Pro subscription for all our products!</div>
+                <div class="chat-bubble chat-bubble-success mb-1">LifeTime support for your product!</div>
+              </div>
+              <p class="text-lg text-error">Drawbacks :</p>
+              <div class="chat chat-start">
+                  <div class="chat-bubble chat-bubble-error mb-1">We own your Feature/Software</div>
+                </div>
+        </div>
+      </div>
+  </div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Intro from "../components/MainPage/Intro.svelte";
     import Services from "../components/MainPage/Services.svelte";
+    import WhyUs from "../components/MainPage/WhyUs.svelte";
 
 </script>
 <Intro/>
+<WhyUs/>
 <Services/>
