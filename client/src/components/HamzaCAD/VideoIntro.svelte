f46d4307f422aedf1f7493827fb0abbaa632c3bf
rayanfarhat <rayanfarhat.sh@gmail.com>
2023-11-15 20:48:02 +0200

    fix video intro padding

--- client/src/components/HamzaCAD/VideoIntro.svelte
+++ client/src/components/HamzaCAD/VideoIntro.svelte
@@ -2,23 +2,28 @@
 	let source="/hamzacad/hamzacad_text.mp4";
 </script>
 
-<div class="diff aspect-[16/9] mt-20 lg:p-16">
-    <div class="diff-item-1">
-            <video width="1280" height="720" autoplay loop muted>
-                <source src="/hamzacad/hamzacad_first_withtext.mp4" type="video/mp4">
-                <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
-            </video>
+<div class="hero bg-base-200 mt-20">
+    <div class="hero-content flex-col-reverse lg:flex-row-reverse">
+        <!-- first col -->
+        <video width="640" height="360" autoplay loop muted>
+            <source src="/hamzacad/hamzacad_first_withtext.mp4" type="video/mp4">
+            <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
+        </video>
+      <!-- second col -->
+      <video width="640" height="360" autoplay loop muted>
+        <source src="/hamzacad/without_hamzacad_withtext.mp4" type="video/mp4">
+        <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
+    </video>
     </div>
-    <div class="diff-item-2">
-            <video width="1280" height="720" autoplay loop muted>
-                <source src="/hamzacad/without_hamzacad_withtext.mp4" type="video/mp4">
-                <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
-            </video>
-    </div>
-    <div class="diff-resizer"></div>
   </div>
 
-<div class="flex items-center flex-col mt-20">
+
+<div class="flex items-center flex-col">
+    
+  <a class="p-0 my-20 hover:bg-transparentt" href="/pricing">
+    <button class="btn btn-primary">{
+      "Subscribe and Download"
+      }</button></a>
     <ul class="menu menu-horizontal bg-base-200 rounded-box">
         <li><button
             on:click={() => source ="/hamzacad/hamzacad_text.mp4"}
@@ -28,12 +33,12 @@
             on:click={() => source ="/hamzacad/hamzacad_lang.mp4"}
             >
             Supporting multiple languages</button></li>
-      </ul>
-{#key source}
-<!-- muted to work more correctly -->
-<video width="1280" height="720" autoplay loop muted>
-    <source src={source} type="video/mp4">
-    <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
-</video>
-{/key}
+    </ul>
+    {#key source}
+        <!-- muted to work more correctly -->
+        <video width="854" height="480" autoplay loop muted>
+            <source src={source} type="video/mp4">
+            <track src="captions_en.vtt" kind="captions" srclang="en" label="english_captions">
+        </video>
+    {/key}
 </div>
