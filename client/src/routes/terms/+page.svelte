0b45a710d1de8ff4ccefdeff3961bde4a336ae5e
rayanfarhat <rayanfarhat.sh@gmail.com>
2023-11-29 21:05:32 +0200

    update Terms of use

--- client/src/routes/terms/+page.svelte
+++ client/src/routes/terms/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
     let terms: Term[];
     type Term = {
         title: string;
@@ -27,6 +25,11 @@
                 "Updates are included for free for as long as your account is active.",
         },
         {
+            title: "Agreement for Unexpected Events and Product Use",
+            paragraph:
+                "In the event of a server crash, database loss, or any unforeseen accidents, you agree that if any of these situations prevent you from using our product, you will not pursue legal action against us under any circumstances.",
+        },
+        {
             title: "Support Services",
             paragraph:
                 "We aim to reply to all support requests. We are not expected to address every request; instead, we prioritize the most frequent and crucial ones, handling them as promptly as we can.",
@@ -34,7 +37,7 @@
         {
             title: "Refunds",
             paragraph:
-                "We acknowledge that our product might not align with everyone's requirements. That's why we provide guided tours before purchase, ensuring clarity about its suitability for you. As our subscription can be to one month, refunds aren't offered due to the short duration and low price.",
+                "We acknowledge that our product might not align with everyone's requirements. That's why we provide guided tours before purchase, ensuring clarity about its suitability for you. As our subscription can be to one month, refunds aren't offered due to the short duration and low price. We will only consider issuing a refund if we have ceased the website and server, discontinued the product, or paused it for several days.",
         },
     ];
 </script>
